7d118274222a1e7c97b2eef0f66e1bedc0071991
Rafael Gómez <rafitagd90@gmail.com>
2018-09-21 19:03:26 +0200

    Add project files.

---
+++ EbookManager.BLL/Ebook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+
+namespace EbookManager
+{
+    class Ebook
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Resume { get; set; }
+        public string Author { get; set; }
+        public string Editorial { get; set; }
+        public int Edition { get; set; }
+        public string Isbn { get; set; }
+        public Image Cover { get; set; }
+        public FileInfo FilePath { get; set; }
+    }
+}
